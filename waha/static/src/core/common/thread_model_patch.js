e61cf86bec413d77b1c86d7e0910a0eddc8416da
pjabie <pjabie@gauchocode.com>
2026-01-02 12:03:13 -0300

    feat: Ensure WhatsApp channels appear in Discuss sidebar

--- waha/static/src/core/common/thread_model_patch.js
+++ waha/static/src/core/common/thread_model_patch.js
@@ -10,4 +10,20 @@ patch(Thread.prototype, {
         }
         return super.channelTypeLabel;
     },
+    
+    get displayToSelf() {
+        // WhatsApp channels should always be visible in sidebar
+        if (this.channel_type === "whatsapp") {
+            return true;
+        }
+        return super.displayToSelf;
+    },
+    
+    get is_channel() {
+        // Treat WhatsApp channels as regular channels for sidebar display
+        if (this.channel_type === "whatsapp") {
+            return true;
+        }
+        return super.is_channel;
+    },
 });
